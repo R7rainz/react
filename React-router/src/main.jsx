d5492a6ca344dd31be47d67ae578dc80b0e1d3c0
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-01-11 16:40:43 +0530

    added loader for gjthub

--- React-router/src/main.jsx
+++ React-router/src/main.jsx
@@ -6,6 +6,7 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import Github from './components/Github/Github.jsx'
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,11 @@ const router = createBrowserRouter([
       {
         path : "contact",
         element : <Contact />
+      },
+      {
+        loader: () => fetch('https://api.github.com/users/R7rainz').then((res) => res.json()),
+        path: "github",
+        element : <Github />
       }
     ]
   }
