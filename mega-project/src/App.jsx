ae8dd7d10133c91a7864ae8abb172f910fe242e9
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-01-16 19:23:30 +0530

    starting working on components

--- mega-project/src/App.jsx
+++ mega-project/src/App.jsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from "./appwrite/auth";
+import { login, logout } from './store/authSlice.js'
+import { Header, Footer } from './components/index.js';
 function App() {
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-     <h1 className='bg-black text-white rounded-3xl border-solid'>BLOG APP WITH APPWRITE</h1> 
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        }
+        else dispatch(logout({ userData }))
+      })
+      .finally(() => setLoading(false))
+  })
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
