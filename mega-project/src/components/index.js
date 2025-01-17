64b3447e320fb225381e78045d0176038740a943
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-01-18 00:57:08 +0530

    login form

--- mega-project/src/components/index.js
+++ mega-project/src/components/index.js
@@ -3,10 +3,15 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx"
+import Button from './Button.jsx'
+import PostCard from './PostCard.jsx'
+
 export {
   Header,
   Footer,
   Container,
   Logo,
-  LogoutBtn
+  LogoutBtn,
+  Button,
+  PostCard
 }
