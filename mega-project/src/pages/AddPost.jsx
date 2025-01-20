94141cfe12c692775c724731704dd22a29846f3b
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-01-20 16:38:21 +0530

    adding pages

---
+++ mega-project/src/pages/AddPost.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PostForm from '../components/post-form/PostForm'
+import Container from './components/Container'
+function AddPost() {
+  return (
+    <div >
+        <Container>
+            <PostForm />
+        </Container>
+    </div>
+  )
+}
+
+export default AddPost
---
+++ mega-project/src/pages/AllPosts.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import service from '../appwrite/config'
+import 
+function AllPosts() {
+  return (
+    <div>AllPosts</div>
+  )
+}
+
+export default AllPosts
