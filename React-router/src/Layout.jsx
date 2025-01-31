2690dfd1c2081141be6ce4e6e6c33d9572c6ce16
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-01-31 23:14:40 +0530

    fixed the header and footer import

--- React-router/src/Layout.jsx
+++ React-router/src/Layout.jsx
@@ -1,14 +1,14 @@
-import Header from '/home/rainz/Documents/react/React-router/src/components/Header/Header.jsx'
-import Footer from '/home/rainz/Documents/react/React-router/src/components/Footer/Footer.jsx'
+import Header from '../src/components/Header/Header'
+import Footer from '../src/components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 function Layout() {
-  return (
-   <>
-    <Header />
-    <Outlet />
-    <Footer />
-   </> 
-  )
+ return (
+  <>
+   <Header />
+   <Outlet />
+   <Footer />
+  </>
+ )
 }
 
 export default Layout
