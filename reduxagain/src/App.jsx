037dcdaf7b08b141dceff7bee49bf6ccf9115ce4
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-02-03 23:33:36 +0530

    started redux again

---
+++ reduxagain/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl bg-gray-700 text-cyan-400'> testing </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
