19fd263777d9da707396dd52bea889da17127ec4
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-02-13 00:55:23 +0530

    backend crashing

--- handlingAPIs/frontend/src/App.tsx
+++ handlingAPIs/frontend/src/App.tsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    (async() => {
+      const response = await axios.get('/api/products')
+      console.log(response.data);
+    })()
+  }, [])
 
   return (
     <>
       <h1>hi there this text is just for testing purposes</h1>
+      <h2>Number of Products are: {products.length}</h2>
     </>
   )
 }
