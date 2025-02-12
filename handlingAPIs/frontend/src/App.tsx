e866fc73dbf8eed89186080b376a7ebf948098d1
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-02-12 22:47:01 +0530

    initialized the frontend

---
+++ handlingAPIs/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>hi there this text is just for testing purposes</h1>
+    </>
+  )
+}
+
+export default App
