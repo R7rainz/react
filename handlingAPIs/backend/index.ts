5f8f30e56c53c1b0e11dd862319c1e933f6a088e
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-02-12 19:22:22 +0530

    debugging...

--- handlingAPIs/backend/index.ts
+++ handlingAPIs/backend/index.ts
@@ -3,37 +3,25 @@ import express from 'express';
 const app = express()
 
 
-app.get('/api/products', (req, res)=>{
+app.get('/api/products', (req, res) => {
+    const products = [
+        { id: 1, name: 'laptop', price: 1000, image: 'laptop.jpg' },
+        { id: 2, name: 'mouse', price: 10, image: 'mouse.jpg' },
+        { id: 3, name: 'keyboard', price: 20, image: 'keyboard.jpg' }
+    ];
 
-    
-    const products = [{
-        id:1,
-        name: 'laptop',
-        price: 1000,
-        image: 'laptop.jpg'
-    },{
-        id:2,
-        name: 'mouse',
-        price: 10,
-        image: 'mouse.jpg'
-    },{
-        id:3,
-        name: 'keyboard',
-        price: 20,
-        image: 'keyboard.jpg'
-    }]
-
-    if(req.query.search){
-        const filterProducts = products.filter(p => p.name.toLowerCase().includes(req.query.search.toString().toLowerCase()));   
-        res.send(filterProducts)
-        return;
+    if (typeof req.query.search === 'string') {
+        const filterProducts = products.filter(p => 
+            p.name.toLowerCase().includes(req.query.search.toLowerCase())
+        );   
+        return res.send(filterProducts);
     }
 
-    setTimeout(() =>{
+    setTimeout(() => {
         res.send(products);
-    }, 3000)
+    }, 3000);
+});
 
-})
 const port = process.env.PORT || 3000;
 
 app.listen(port, ()=>{
