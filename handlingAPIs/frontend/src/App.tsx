60d5fce3294d17b400d99bce50414606d004f381
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-02-13 23:13:23 +0530

    created a function separately

--- handlingAPIs/frontend/src/App.tsx
+++ handlingAPIs/frontend/src/App.tsx
@@ -3,22 +3,46 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
+  const [products, error, loading] = CustomReactQuery('api/products')
 
-  const [products, setProducts] = useState([])
+  if (error) {
+    return <h1>Something Went Wrong</h1>
+  }
 
-  useEffect(() => {
-    (async() => {
-      const response = await axios.get('/api/products')
-      console.log(response.data);
-    })()
-  }, [])
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
 
   return (
     <>
-      <h1>hi there this text is just for testing purposes</h1>
-      <h2>Number of Products are: {products.length}</h2>
+      <h1>Product List</h1>
+      <h2>Number of Products: {products.length}</h2>
     </>
   )
 }
 
 export default App
+
+function CustomReactQuery(urlPath: string) {
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true)
+        setError(false)
+        const response = await axios.get(urlPath)
+        console.log(response.data)
+        setProducts(response.data)
+        setLoading(false)
+      } catch (error) {
+        setError(true)
+        setLoading(false)
+      }
+    })()
+  }, [urlPath])
+
+  return [products, error, loading] as const
+}
