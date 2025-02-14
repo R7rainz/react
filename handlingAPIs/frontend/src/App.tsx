00e81ae14c8c7bbbf70b63c9c485d0ad7b41fb77
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-02-14 10:09:30 +0530

    completed

--- handlingAPIs/frontend/src/App.tsx
+++ handlingAPIs/frontend/src/App.tsx
@@ -3,19 +3,19 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
-  const [products, error, loading] = CustomReactQuery('api/products')
-
-  if (error) {
-    return <h1>Something Went Wrong</h1>
-  }
-
-  if (loading) {
-    return <h1>Loading...</h1>
-  }
+  const [products, error, loading, search, setSearch] = CustomReactQuery('api/products?search=')
 
   return (
     <>
       <h1>Product List</h1>
+      <input type="text" placeholder="Search..." 
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Something Went Wrong</h1>}
+
       <h2>Number of Products: {products.length}</h2>
     </>
   )
@@ -27,22 +27,35 @@ function CustomReactQuery(urlPath: string) {
   const [products, setProducts] = useState([])
   const [error, setError] = useState<boolean>(false)
   const [loading, setLoading] = useState<boolean>(true)
+  const [search, setSearch] = useState<string>('')
 
   useEffect(() => {
-    (async () => {
+    const controller = new AbortController()
+    ;(async () => {
       try {
         setLoading(true)
         setError(false)
-        const response = await axios.get(urlPath)
+        const response = await axios.get(urlPath+search, {
+          signal: controller.signal
+        })
         console.log(response.data)
         setProducts(response.data)
         setLoading(false)
       } catch (error) {
+        if(axios.isCancel(error)) {
+          console.log('Request Cancelled', error.message)
+          return
+        }
         setError(true)
         setLoading(false)
       }
     })()
-  }, [urlPath])
 
-  return [products, error, loading] as const
+    //cleanup code
+    return () => {
+      controller.abort()
+    }
+  }, [urlPath, search])
+
+  return [products, error, loading, search, setSearch] as const
 }
